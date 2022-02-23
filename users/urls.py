384bdf99814cff5ff003c899ef0b6a6c7f66e9bb
Aman Chourasiya <2019csb1067@iitrpr.ac.in>
2022-02-24 01:07:55 +0530

    home page

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('login/', views.LoginView, name = 'LoginView'),
+    path('', views.HomeView, name = 'HomeView'),
 ]
--- users/views.py
+++ users/views.py
@@ -10,5 +10,22 @@ from django.http import JsonResponse
 from django.core.files.storage import FileSystemStorage
 from django.core import serializers
 
+def HomeView(request):
+    return render(request, 'home/index.html')
+    
 def LoginView(request):
-    return render(request, 'users/login.html')
+    if(request.user.is_authenticated):
+        return render(request, 'main/home.html')
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        if(email != "" and password != ""):
+            user = authenticate(username = email, password = password)
+            if(user != None):
+                login(request, user)
+                return redirect('HomeView')
+            else:
+                return render(request, 'users/login.html', {'code': '1', 'msg': 'Invalid Credentials.'})
+        else:
+            return render(request, 'users/login.html', {'code' : '1', 'msg': 'Fields cannot be empty.'})
+    return render(request, 'users/login.html', {'code' : '0'})
