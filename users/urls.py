502d176d2e0ddb55ad3bf0b4fa40259545d7be2d
Het <62541263+HETFADIA@users.noreply.github.com>
2022-02-27 13:56:25 +0530

    upd

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('login/', views.LoginView, name = 'LoginView'),
     path('logout/', views.LogoutView, name = 'LogoutView'),
     path('register/', views.RegisterView, name = 'RegisterView'),
+    path('passwordreset/',views.PasswordResetView, name = 'PasswordResetView'),
     path('', views.HomeView, name = 'HomeView'),
 ]
