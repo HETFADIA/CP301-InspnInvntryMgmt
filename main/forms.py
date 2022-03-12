1292f4bd8d97ee4c1f432ee0444ec143b1fe0d20
Het <62541263+HETFADIA@users.noreply.github.com>
2022-03-13 02:57:28 +0530

    form_style_upd_small

--- main/forms.py
+++ main/forms.py
@@ -17,4 +17,17 @@ class InvoiceModelForm(forms.ModelForm):
             'invoice_amount',
             'invoice_number',
         ]
+        widgets={
+            'ir_number':forms.NumberInput(attrs={'class':'form-control'}),
+            'ir_date': forms.TextInput(attrs={'class':'form-control'}),
+            'department': forms.TextInput(attrs={'class':'form-control'}),
+            'vendorName': forms.TextInput(attrs={'class':'form-control'}),
+            'po_number': forms.TextInput(attrs={'class':'form-control'}),
+            'po_date': forms.TextInput(attrs={'class':'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class':'form-control'}),
+            'rate': forms.NumberInput(attrs={'class':'form-control'}),
+            'amount': forms.NumberInput(attrs={'class':'form-control'}),
+            'invoice_amount': forms.NumberInput(attrs={'class':'form-control'}),
+            'invoice_number': forms.NumberInput(attrs={'class':'form-control'})
+        }
     
