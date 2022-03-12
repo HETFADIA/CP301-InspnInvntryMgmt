9eb97c1506614f888dd1f56b7507edb48dae66c7
Het <62541263+HETFADIA@users.noreply.github.com>
2022-03-12 18:05:59 +0530

    update

--- main/views.py
+++ main/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
@@ -12,11 +13,14 @@ def fill_Invoice(request):
         form = InvoiceModelForm()
 
         if request.method == 'POST':
-            print("aa rha h....aaaa rhaa h...data aa rha h....🤩")
+            form = InvoiceModelForm(request.POST)
+
+            if form.is_valid():
+                print("aa rha h....aaaa rhaa h...data aa rha h....🤩")
+                return HttpResponse("...kamal kr diya..aapne form bharke...aapko pore do joote milte h...innaam me!")
 
         context = {'form': form}
         return render(request, 'main/newinvoice.html', context)
     else:
         # TODO: message to be changed
-        return HttpResponse("kaha ghuse chle aa rhe ho bhsdk!!..login kro pehle beta!")
-    
+        return HttpResponse("kaha ghuse chle aa rhe ho!!..login kro pehle beta!")
